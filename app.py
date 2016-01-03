b9b88dcbff150c87ea369feab028b1d0a4cdd777
sharpdeep <1713715949@qq.com>
2016-01-03 23:11:23 +0800

    add logger

--- app.py
+++ app.py
@@ -7,6 +7,18 @@
 @time: 2016-01-01 23:11
 """
 from flask import Flask
+from conf.config import configs
+import logging
+from  logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(configs.app.log.name)
+logfileHandler = RotatingFileHandler(configs.app.log.logFile,
+                                     maxBytes=configs.app.log.maxBytes,
+                                     backupCount=configs.app.log.backupCount)
+logfileHandler.setFormatter(logging.Formatter('%(asctime)s  %(levelname)s  %(message)s'))
+logfileHandler.setLevel(configs.app.log.logLevel)
+logger.addHandler(logfileHandler)
+logger.setLevel(configs.app.log.logLevel)
 
 
 app = Flask(__name__)
--- conf/config_default.py
+++ conf/config_default.py
@@ -7,7 +7,25 @@
 @time: 2016-01-01 21:39
 """
 
+import logging
+import os
+
+
+
 configs = {
+    'app':{
+        'debug':True,
+        'host':'0.0.0.0',
+        'port':5000,
+        'log':{
+            'name':'main',
+            'logLevel':logging.INFO,
+            'logFile':os.path.abspath('./log/main.log'),
+            'maxBytes':5*1024*1024,
+            'backupCount':50,
+        }
+    },
+
     'db':{
         'host':'127.0.0.1',
         'port':3306,
