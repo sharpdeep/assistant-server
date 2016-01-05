803bf04faba0b9d9c255fef7b6955a68a61ec1ed
sharpdeep <1713715949@qq.com>
2016-01-05 19:27:59 +0800

    将success和failed的默认类型由dict改为Dict

--- core/status.py
+++ core/status.py
@@ -3,14 +3,15 @@
 
 """
 @author: sharpdeep
-@file:state.py
+@file:status.py
 @time: 2016-01-05 16:07
 @功能：规范化返回，status表示执行成功或失败，msg表示相应的信息，data表示返回的数据，推荐dict
 """
 from enum import Enum,unique
+from conf.config import Dict,toDict
 
 @unique
-class State(Enum):
+class Status(Enum):
     SUCCESS = 'success'
     FAILED = 'failed'
     ERROR = 'error'
@@ -22,38 +23,41 @@ class Error(Enum):
 def success(msg='',data={}):
     if data:
         return {
-            'status':State.SUCCESS,
+            'status':Status.SUCCESS,
             'msg':msg,
             'data':data
         }
     else:
         return {
-            'status':State.SUCCESS,
+            'status':Status.SUCCESS,
             'msg':msg
         }
 
 def failed(msg='',data={}):
     if data:
-        return {
-            'status':State.FAILED,
+        ret_val =  {
+            'status':Status.FAILED,
             'msg':msg,
             'data':data
         }
     else:
-        return {
-            'status':State.FAILED,
+        ret_val =  {
+            'status':Status.FAILED,
             'msg':msg
         }
 
+    return toDict(ret_val)
+
 def error(msg = '',data={}):
     if data:
-        return {
-            'status':State.ERROR,
+        ret_val =  {
+            'status':Status.ERROR,
             'msg':msg,
             'data':data
         }
     else:
-        return {
-            'status':State.ERROR,
+        ret_val =  {
+            'status':Status.ERROR,
             'msg':msg
         }
+    return toDict(ret_val)
