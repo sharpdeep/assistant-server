37fe34dfb4a49f8678ba7c72abb2db51625fb8f0
sharpdeep <1713715949@qq.com>
2016-01-05 19:28:54 +0800

    将urllib改为第三方requests模块

--- core/util.py
+++ core/util.py
@@ -6,11 +6,11 @@
 @file:util.py
 @time: 2016-01-02 00:19
 """
-from urllib import parse
-from urllib import request
 from conf.config import configs
-from http import cookiejar
-from core.state import *
+from core.status import *
+from datetime import datetime
+from enum import Enum,unique
+import requests
 import logging
 
 logger = logging.getLogger(configs.app.log.name)
@@ -19,44 +19,27 @@ login_host = 'http://credit.stu.edu.cn/portal/stulogin.aspx'
 website_encoding = 'gbk'
 login_timeout = 3 #seconds
 
+@unique
+class semester(Enum):
+    AUTUMN = 0
+    SPRING = 1
+    SUMMER = 2
+
 def authenticate(username,passwd,login=False):
-    """
-    利用学分制验证用户
-    :param username:
-    :param passwd:
-    :return:True->验证成功，False->验证失败，None->error
-    """
-    postdata = parse.urlencode({
+    postdata = {
         "txtUserID": username,
         "txtUserPwd": passwd,
         "btnLogon": "登录",
         '__VIEWSTATE': '/wEPDwUKMTM1MzI1Njg5N2Rk47x7/EAaT/4MwkLGxreXh8mHHxA=',
         '__EVENTVALIDATION': '/wEWBAKo25zdBALT8dy8BQLG8eCkDwKk07qFCRXt1F3RFYVdjuYasktKIhLnziqd', #aspx坑爹的地方
-    }).encode('utf-8')
-
-    if login:
-        cookie_jar = cookiejar.CookieJar()
-        opener = request.build_opener(request.HTTPCookieProcessor(cookie_jar))
-        try:
-            resp = opener.open(login_host,postdata,login_timeout)
-        except Exception as e:
-            print(e)
-            return error(Error.CONNECT_ERROR)
-        content = resp.read()
-        if content.__contains__(b'alert'):
-            return failed('验证失败')
-        else:
-            return success('登陆成功',data={'opener':opener})
-    else:
-        try:
-            resp = request.urlopen(login_host,postdata,login_timeout)
-        except Exception as e:
-            return error(Error.CONNECT_ERROR)
-        content = resp.read()
-        if content.__contains__(b'alert'):
-            return failed('验证失败')
-        else:
-            return success('验证成功')
-
-
+    }
+    try:
+        resp = requests.post(login_host,postdata,login_timeout)
+    except Exception as e:
+        return error(Error.CONNECT_ERROR)
+    if resp.content.__contains__(b'alert'):
+        return failed('验证失败')
+    if not login:
+        return success('验证成功')
+    return success('登陆成功',data={'cookie':resp.cookies})
 
